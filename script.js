9bfbf725abc4929bf7003cd668650d057be79e11
Vedant <vedantparag123@gmail.com>
2026-03-07 16:15:12 +0530

    Piano

--- script.js
+++ script.js
@@ -1,21 +1,64 @@
-var btn = document.querySelector("button");
-var h2 = document.querySelector("h2");
-var inner = document.querySelector(".inner");
-var bottom = document.querySelector("bottom");
-var grow= 0;
-var time= Math.floor(Math.random()*30)+50
+let A = new Audio('28.mp3');
+let B = new Audio('29.mp3');
+let C = new Audio('30.mp3');
+let D = new Audio('31.mp3');
+let E = new Audio('32.mp3');
+let F = new Audio('33.mp3');
+let G = new Audio('34.mp3');
+let H = new Audio('35.mp3');
+let I = new Audio('36.mp3');
+let J = new Audio('37.mp3');
+let K = new Audio('38.mp3');
+let L = new Audio('39.mp3');
+let M = new Audio('40.mp3');
 
-btn.addEventListener("click", function(){
-    var bar = setInterval(() => {
-        grow++
-        h2.innerHTML=grow+'%'
-        inner.style.width=grow+'%'
-        btn.style.pointerEvents='none'
-        btn.style.opacity='70%'
-    }, time);
-    setTimeout(() => {
-        clearInterval(bar)
-        var para = document.createElement("p");
-    }, time*100);
-    console.log(time)
+var h2 = document.querySelector("h2")
+var main = document.querySelector("body");
+main.addEventListener('keydown', function(elem){
+    // console.log(elem.key)
+    if(elem.key=='A'||elem.key=='a'){
+        A.play();
+
+    }
+    else if(elem.key=='B'||elem.key=='b'){
+        B.play();
+    }
+    else if(elem.key=='C'||elem.key=='c'){
+        C.play();
+    }
+    else if(elem.key=='D'||elem.key=='d'){
+        D.play();
+    }
+    else if(elem.key=='E'||elem.key=='e'){
+        E.play();
+    }
+    else if(elem.key=='F'||elem.key=='f'){
+        F.play();
+    }
+    else if(elem.key=='G'||elem.key=='g'){
+        G.play();
+    }
+    else if(elem.key=='H'||elem.key=='h'){
+        H.play();
+    }
+    else if(elem.key=='I'||elem.key=='i'){
+        I.play();
+    }
+    else if(elem.key=='J'||elem.key=='j'){
+        J.play();
+    }
+    else if(elem.key=='K'||elem.key=='k'){
+        K.play();
+    }
+    else if(elem.key=='L'||elem.key=='l'){
+        L.play();
+    }
+    else if(elem.key=='M'||elem.key=='m'){
+        M.play();
+    }
+
 })
+
+
+
+
